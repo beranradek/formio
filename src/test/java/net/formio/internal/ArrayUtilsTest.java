5446fb7bdcf4bdfc9324338869cfa97b1ca491b4
Radek Beran <beran.radek@seznam.cz>
2014-12-28 21:05:35 +0100

    ArrayUtilsTest

---
+++ src/test/java/net/formio/internal/ArrayUtilsTest.java
@@ -0,0 +1,39 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.formio.internal;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+/**
+ * Tests for {@link ArrayUtils}.
+ * @author Radek Beran
+ *
+ */
+public class ArrayUtilsTest {
+
+	@Test
+	public void testConvertPrimitiveArrayToList() {
+		assertEquals(Integer.valueOf(2), ArrayUtils.convertPrimitiveArrayToList(new int[] { 1, 2 }).get(1));
+		assertEquals(Boolean.TRUE, ArrayUtils.convertPrimitiveArrayToList(new boolean[] { false, true }).get(1));
+		assertEquals(Byte.valueOf((byte)2), ArrayUtils.convertPrimitiveArrayToList(new byte[] { 1, 2 }).get(1));
+		assertEquals(Short.valueOf((short)2), ArrayUtils.convertPrimitiveArrayToList(new short[] { 1, 2 }).get(1));
+		assertEquals(Long.valueOf(2L), ArrayUtils.convertPrimitiveArrayToList(new long[] { 1L, 2L }).get(1));
+	}
+
+}
